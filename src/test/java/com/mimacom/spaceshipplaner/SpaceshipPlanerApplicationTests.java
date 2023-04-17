fae8c1ddc75cc6e094c764deca82a721e31abc8b
Baris Cubukcuoglu <cubukcuoglu.baris@gmail.com>
2023-04-17 10:31:32 +0200

    Inital commit

---
+++ src/test/java/com/mimacom/spaceshipplaner/SpaceshipPlanerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mimacom.spaceshipplaner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceshipPlanerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
